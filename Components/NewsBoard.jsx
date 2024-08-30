339272a8b7c1f6e6c029a764667841c7dbc54e8a
chayanc7mondal <mitalichayan18082004@gmail.com>
2024-08-31 00:06:24 +0530

    fixing

---
+++ Components/NewsBoard.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import NewsItem from './NewsItem';
+
+const NewsBoard = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_KEY}`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setArticles(data.articles))
+      .catch((error) => console.error("Error fetching news:", error));
+  }, []);
+
+  return (
+    <div>
+      <h2 className="text-center">
+        Latest <span className="badge bg-danger">News</span>
+      </h2>
+      <div className="news-container">
+        {articles.map((news, index) => (
+          <NewsItem
+            key={index}
+            title={news.title}
+            description={news.description}
+            src={news.urlToImage}
+            url={news.url}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default NewsBoard;
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../Components/Navbar"
+import NewsBoard from "../Components/NewsBoard"
+import NewsItem from "../Components/NewsItem"
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <NewsBoard />
+      <NewsItem />
+    </div>
+  )
+}
+
+export default App
